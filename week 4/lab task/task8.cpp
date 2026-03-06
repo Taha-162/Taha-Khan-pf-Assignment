39176a3adab9d2a1de1bfdae2fdc9fc2ef8052d0
Taha Khan <talha1786@gmail.com>
2026-03-06 20:11:40 +0500

    Add third file

---
+++ week 4/lab task/task8.cpp
@@ -0,0 +1,156 @@
+#include <iostream>
+using namespace std;
+main()
+{
+    char lowercase;
+
+    cout << "Enter any character in lower case :";
+    cin >> lowercase;
+
+    if (lowercase == 'a')
+    {
+        cout << "vowel";
+    }
+    if (lowercase == 'e')
+    {
+        cout << "vowel";
+    }
+    if (lowercase == 'i')
+    {
+        cout << "vowel";
+    }
+    if (lowercase == 'o')
+    {
+        cout << "vowel";
+    }
+    if (lowercase == 'u')
+    {
+        cout << "vowel";
+    }
+
+    if (lowercase == 'b')
+    {
+        cout << "consonant";
+    }
+    if (lowercase == 'c')
+    {
+        cout << "consonant";
+    }
+    if (lowercase == 'd')
+    {
+        cout << "consonant";
+    }
+    if (lowercase == 'f')
+    {
+        cout << "consonant";
+    }
+    if (lowercase == 'g')
+    {
+        cout << "consonant";
+    }
+    if (lowercase == 'h')
+    {
+        cout << "consonant";
+    }
+    if (lowercase == 'j')
+    {
+        cout << "consonant";
+    }
+    if (lowercase == 'k')
+    {
+        cout << "consonant";
+    }
+    if (lowercase == 'l')
+    {
+        cout << "consonant";
+    }
+    if (lowercase == 'm')
+    {
+        cout << "consonant";
+    }
+    if (lowercase == 'n')
+    {
+        cout << "consonant";
+    }
+    if (lowercase == 'p')
+    {
+        cout << "consonant";
+    }
+    if (lowercase == 'q')
+    {
+        cout << "consonant";
+    }
+    if (lowercase == 'r')
+    {
+        cout << "consonant";
+    }
+    if (lowercase == 's')
+    {
+        cout << "consonant";
+    }
+    if (lowercase == 't')
+    {
+        cout << "consonant";
+    }
+    if (lowercase == 'v')
+    {
+        cout << "consonant";
+    }
+    if (lowercase == 'w')
+    {
+        cout << "consonant";
+    }
+    if (lowercase == 'x')
+    {
+        cout << "consonant";
+    }
+    if (lowercase == 'y')
+    {
+        cout << "consonant";
+    }
+    if (lowercase == 'z')
+    {
+        cout << "consonant";
+    }
+
+    if (lowercase == 0)
+    {
+        cout << "it is a number";
+    }
+    if (lowercase == 1)
+    {
+        cout << "it is a number";
+    }
+    if (lowercase == 2)
+    {
+        cout << "it is a number";
+    }
+    if (lowercase == 3)
+    {
+        cout << "it is a number";
+    }
+    if (lowercase == 4)
+    {
+        cout << "it is a number";
+    }
+    if (lowercase == 5)
+    {
+        cout << "it is a number";
+    }
+    if (lowercase == 6)
+    {
+        cout << "it is a number";
+    }
+    if (lowercase == 7)
+    {
+        cout << "it is a number";
+    }
+    if (lowercase == 8)
+    {
+        cout << "it is a number";
+    }
+    if (lowercase == 9)
+    {
+        cout << "it is a number";
+    }
+}
